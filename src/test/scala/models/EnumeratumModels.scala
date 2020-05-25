1818ee4fbe1c34958b4603453432068dd9be3b4c
PJ Fanning <pj@kontainers.co.uk>
2020-05-25 15:51:04 +0200

    fix compilation of test (scala 2.11 issue)

--- src/test/scala/models/EnumeratumModels.scala
+++ src/test/scala/models/EnumeratumModels.scala
@@ -7,7 +7,7 @@ case class SModelWithEnum(orderSize: OrderSize = OrderSize.TALL)
 sealed abstract class OrderSize extends EnumEntry
 
 object OrderSize extends Enum[OrderSize] {
-  override def values: IndexedSeq[OrderSize] = findValues
+  override def values: scala.collection.immutable.IndexedSeq[OrderSize] = findValues
 
   case object TALL extends OrderSize
   case object GRANDE extends OrderSize

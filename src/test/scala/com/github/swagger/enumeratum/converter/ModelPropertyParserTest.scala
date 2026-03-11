f38fb26ccac8f72b4e25dd0f1ac00bb37fb0b4ae
PJ Fanning <pjfanning@users.noreply.github.com>
2026-03-11 03:31:55 +0100

    Update swagger-scala-module version to 2.15.0 (#177)
    * Update swagger-scala-module version to 2.15.0
    
    * test issue

--- src/test/scala/com/github/swagger/enumeratum/converter/ModelPropertyParserTest.scala
+++ src/test/scala/com/github/swagger/enumeratum/converter/ModelPropertyParserTest.scala
@@ -119,7 +119,8 @@ class ModelPropertyParserTest extends AnyFlatSpec with Matchers with OptionValue
     val schema = field.asInstanceOf[StringSchema]
     schema.getDescription shouldEqual "An annotated field"
     schema.getName shouldEqual "field"
-    schema.getDefault should be(null)
+    // https://github.com/swagger-api/swagger-core/pull/5063 explains the default value
+    schema.getDefault shouldEqual "##default"
     schema.getDeprecated should be(null)
     nullSafeList(schema.getEnum) shouldEqual Ctx.Color.values.map(_.entryName)
     nullSafeList(model.value.getRequired) shouldBe Seq("field")
